04a4528ca4d15f932eee0cbe910a04e16e7768e5
Pradeep Devka <pradeep.devka@stockgro.com>
2026-06-23 10:16:56 +0530

    list append method

--- basics/lists.py
+++ basics/lists.py
@@ -1,3 +1,10 @@
 lang =["Java","Python","TypeScript"]
 for lang in lang:
     print(lang)
+
+
+
+fruits = ["Apple", "Mango", "Orange"]
+fruits.append("Banana")
+for fruit in fruits:
+    print(fruit)
